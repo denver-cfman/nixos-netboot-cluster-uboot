d3114368f9f72619af067f9cadb84e158272b54d
Mr Z <denver.cfman@gmail.com>
2026-06-20 19:29:28 -0600

    Update flake.nix for U-Boot cross-compilation

--- flake.nix
+++ flake.nix
@@ -1,23 +1,34 @@
 {
-  description = "Cross-compile U-Boot for Raspberry Pi";
+  description = "Cross-compile U-Boot and prepare SD images";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux = let
-      pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnsupportedSystem = true; };
+      pkgsFor = crossSystem: import nixpkgs {
+        inherit crossSystem;
+        system = "x86_64-linux";
+        config.allowUnsupportedSystem = true;
+      };
     in {
-      # Use specific 64-bit targets for aarch64
-      uboot-aarch64 = pkgs.pkgsCross.aarch64-multiplatform.ubootRaspberryPi4_64bit;
-      
-      # For 32-bit targets, use the appropriate RPi 32-bit defconfigs
-      uboot-armv7 = pkgs.pkgsCross.armv7l-hf.ubootRaspberryPi3_32bit;
+      uboot-armv6   = (pkgsFor { config = "armv6l-unknown-linux-gnueabihf"; }).buildPackages.ubootRaspberryPi;
+      uboot-armv7   = (pkgsFor { config = "armv7l-unknown-linux-gnueabihf"; }).buildPackages.ubootRaspberryPi;
+      uboot-aarch64 = (pkgsFor { config = "aarch64-unknown-linux-gnu"; }).buildPackages.ubootRaspberryPi;
     };
 
     devShells.x86_64-linux.default = let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in pkgs.mkShell {
-      buildInputs = with pkgs; [ dtc mtools parted ];
+      buildInputs = with pkgs; [
+        dtc          # Device Tree Compiler
+        mtools       # Tools for manipulating FAT images
+        parted       # Disk partitioning
+        gcc          # Host compiler
+        gnumake      # Build automation
+      ];
+      shellHook = ''
+        echo "Ready to build U-Boot and prepare SD card images."
+      '';
     };
   };
 }
