78c37dda981d0bba080bd10174921b37c838b950
Mr Z <denver.cfman@gmail.com>
2026-06-23 01:17:45 -0600

    Refactor U-Boot configurations and SD image exports

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     packages.x86_64-linux = let
       pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnsupportedSystem = true; };
       
+      # Driver configuration helper
       withUsbEthernet = uboot: uboot.overrideAttrs (old: {
         postConfigure = (old.postConfigure or "") + ''
           cat >> .config <<EOF
@@ -14,9 +15,7 @@
           CONFIG_USB_ETHER=y
           CONFIG_USB_ETHER_ASIX=y
           CONFIG_USB_ETHER_ASIX88179=y
-          # Required for Pi 4/5 USB/PCIe support
-          CONFIG_PCI=y
-          CONFIG_PCI_BCM2835=y
+          CONFIG_USB_DWC2=y
           CONFIG_USB_XHCI_HCD=y
           CONFIG_USB_XHCI_PCI=y
           EOF
@@ -24,10 +23,11 @@
         '';
       });
 
-      # Define custom U-Boot versions
-      ubootAarch64 = withUsbEthernet pkgs.pkgsCross.aarch64-multiplatform.ubootRaspberryPi4_64bit;
-      ubootArmv7   = withUsbEthernet pkgs.pkgsCross.armv7l-hf-multiplatform.ubootRaspberryPi3_32bit;
-      ubootArmv6   = withUsbEthernet pkgs.pkgsCross.raspberryPi.ubootRaspberryPi;
+      # Architecture specific U-Boot targets
+      ubootArmv6  = withUsbEthernet pkgs.pkgsCross.raspberryPi.ubootRaspberryPi;
+      ubootArmv7  = withUsbEthernet pkgs.pkgsCross.armv7l-hf-multiplatform.ubootRaspberryPi3_32bit;
+      ubootArmv8  = withUsbEthernet pkgs.pkgsCross.aarch64-multiplatform.ubootRaspberryPi3_64bit; # Pi Zero 2 W / Pi 3
+      ubootPi4    = withUsbEthernet pkgs.pkgsCross.aarch64-multiplatform.ubootRaspberryPi4_64bit; # Pi 4 / 5
 
       mkSDImage = { uboot, configTxt, bootCmd }: pkgs.stdenv.mkDerivation {
         name = "rpi-sd-image-${uboot.name}";
@@ -37,18 +37,12 @@
           mkdir -p $out
           truncate -s 120M $out/sd-image.img
           ${pkgs.mtools}/bin/mformat -i $out/sd-image.img -F -v "BOOT" ::
-          
           mkdir -p stage
-          # Compile boot script
           ${pkgs.ubootTools}/bin/mkimage -A arm -O linux -T script -C none -n "Boot Script" -d $BOOT_CMD stage/boot.scr
-          
-          # Copy firmware and assets
           cp ${pkgs.raspberrypifw}/share/raspberrypi/boot/{bootcode.bin,start.elf,fixup.dat,*.dtb} stage/
           cp -r ${pkgs.raspberrypifw}/share/raspberrypi/boot/overlays stage/
           cp ${uboot}/u-boot.bin stage/kernel.img
           echo "${configTxt}" > stage/config.txt
-          
-          # Copy to image with debug trace
           set -x
           for file in stage/*; do
             ${pkgs.mtools}/bin/mcopy -i $out/sd-image.img "$file" ::
@@ -56,27 +50,11 @@
         '';
       };
     in {
-      # Export custom U-Boot binaries
-      uboot-aarch64 = ubootAarch64;
-      uboot-armv7   = ubootArmv7;
-      uboot-armv6   = ubootArmv6;
-
       # Export SD Images
-      image-aarch64 = mkSDImage { 
-        uboot = ubootAarch64;
-        configTxt = "kernel=kernel.img\nenable_uart=1\narm_64bit=1\ndtoverlay=dwc2,dr_mode=host";
-        bootCmd = ./boot.cmd;
-      };
-      image-armv7 = mkSDImage { 
-        uboot = ubootArmv7;
-        configTxt = "kernel=kernel.img\nenable_uart=1\narm_64bit=0\ndtoverlay=dwc2,dr_mode=host";
-        bootCmd = ./boot.cmd;
-      };
-      image-armv6 = mkSDImage { 
-        uboot = ubootArmv6;
-        configTxt = "kernel=kernel.img\nenable_uart=1\narm_64bit=0\ndtoverlay=dwc2,dr_mode=host";
-        bootCmd = ./boot.cmd;
-      };
+      image-armv6     = mkSDImage { uboot = ubootArmv6; configTxt = "kernel=kernel.img\nenable_uart=1\narm_64bit=0\ndtoverlay=dwc2,dr_mode=host"; bootCmd = ./boot.cmd; };
+      image-armv7     = mkSDImage { uboot = ubootArmv7; configTxt = "kernel=kernel.img\nenable_uart=1\narm_64bit=0\ndtoverlay=dwc2,dr_mode=host"; bootCmd = ./boot.cmd; };
+      image-armv8-rpi3 = mkSDImage { uboot = ubootArmv8; configTxt = "kernel=kernel.img\nenable_uart=1\narm_64bit=1\ndtoverlay=dwc2,dr_mode=host"; bootCmd = ./boot.cmd; };
+      image-rpi4-5     = mkSDImage { uboot = ubootPi4;    configTxt = "kernel=kernel.img\nenable_uart=1\narm_64bit=1\ndtoverlay=dwc2,dr_mode=host"; bootCmd = ./boot.cmd; };
     };
   };
 }
