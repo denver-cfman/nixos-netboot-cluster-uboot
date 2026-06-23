b92c7d4c94b8fd3fc6648ae10350878880cf4fb7
Mr Z <denver.cfman@gmail.com>
2026-06-22 19:30:04 -0600

    Enhance U-Boot with USB Ethernet support
    Added support for USB Ethernet in U-Boot configuration.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Cross-compile U-Boot and prepare SD images";
+  description = "Cross-compile U-Boot and prepare SD images with USB Ethernet support";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -7,56 +7,60 @@
     packages.x86_64-linux = let
       pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnsupportedSystem = true; };
       
+      # Function to apply USB Ethernet support to ANY U-Boot package
+      withUsbEthernet = uboot: uboot.overrideAttrs (old: {
+        postConfigure = (old.postConfigure or "") + ''
+          echo "CONFIG_USB_ETHER_ASIX=y" >> .config
+          echo "CONFIG_USB_ETHER_ASIX88179=y" >> .config
+          make oldconfig
+        '';
+      });
+
       mkSDImage = { uboot, configTxt, bootCmd }: pkgs.stdenv.mkDerivation {
         name = "rpi-sd-image-${uboot.name}";
         nativeBuildInputs = [ pkgs.mtools pkgs.ubootTools pkgs.libfaketime ];
-        BOOT_CMD = bootCmd; 
-         buildCommand = ''
+        BOOT_CMD = bootCmd;
+        buildCommand = ''
           mkdir -p $out
           truncate -s 120M $out/sd-image.img
           ${pkgs.mtools}/bin/mformat -i $out/sd-image.img -F -v "BOOT" ::
+          
           mkdir -p stage
-          cp $BOOT_CMD stage/boot.cmd
-          ${pkgs.ubootTools}/bin/mkimage -A arm -O linux -T script -C none -n "Boot Script" -d stage/boot.cmd stage/boot.scr
+          ${pkgs.ubootTools}/bin/mkimage -A arm -O linux -T script -C none -n "Boot Script" -d $BOOT_CMD stage/boot.scr
+          
           cp ${pkgs.raspberrypifw}/share/raspberrypi/boot/{bootcode.bin,start.elf,fixup.dat,*.dtb} stage/
           cp -r ${pkgs.raspberrypifw}/share/raspberrypi/boot/overlays stage/
           cp ${uboot}/u-boot.bin stage/kernel.img
           echo "${configTxt}" > stage/config.txt
-          set -x 
+          
+          set -x
           for file in stage/*; do
             ${pkgs.mtools}/bin/mcopy -i $out/sd-image.img "$file" ::
           done
         '';
       };
     in {
-      # Raw U-Boot binaries
-      #uboot-aarch64 = pkgs.pkgsCross.aarch64-multiplatform.ubootRaspberryPi4_64bit;
-      #uboot-armv7   = pkgs.pkgsCross.armv7l-hf-multiplatform.ubootRaspberryPi3_32bit;
-      #uboot-armv6   = pkgs.pkgsCross.raspberryPi.ubootRaspberryPi;
+      # Custom U-Boot packages
+      uboot-aarch64 = withUsbEthernet pkgs.pkgsCross.aarch64-multiplatform.ubootRaspberryPi4_64bit;
+      uboot-armv7   = withUsbEthernet pkgs.pkgsCross.armv7l-hf-multiplatform.ubootRaspberryPi3_32bit;
+      uboot-armv6   = withUsbEthernet pkgs.pkgsCross.raspberryPi.ubootRaspberryPi;
 
-      # SD Images
+      # SD Images using the custom U-Boot
       image-aarch64 = mkSDImage { 
-        uboot = pkgs.pkgsCross.aarch64-multiplatform.ubootRaspberryPi4_64bit;
+        uboot = self.packages.x86_64-linux.uboot-aarch64;
         configTxt = "kernel=kernel.img\nenable_uart=1\narm_64bit=1";
         bootCmd = ./boot.cmd;
       };
       image-armv7 = mkSDImage { 
-        uboot = pkgs.pkgsCross.armv7l-hf-multiplatform.ubootRaspberryPi3_32bit;
+        uboot = self.packages.x86_64-linux.uboot-armv7;
         configTxt = "kernel=kernel.img\nenable_uart=1\narm_64bit=0";
         bootCmd = ./boot.cmd;
       };
       image-armv6 = mkSDImage { 
-        uboot = pkgs.pkgsCross.raspberryPi.ubootRaspberryPi;
+        uboot = self.packages.x86_64-linux.uboot-armv6;
         configTxt = "kernel=kernel.img\nenable_uart=1\narm_64bit=0";
         bootCmd = ./boot.cmd;
       };
-    };
-
-    devShells.x86_64-linux.default = let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in pkgs.mkShell {
-      buildInputs = with pkgs; [ dtc mtools parted gcc gnumake ];
-      shellHook = "echo 'Ready to build U-Boot and SD images.'";
     };
   };
 }
