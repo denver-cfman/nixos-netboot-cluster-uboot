d23b1717863fcf4c30d79a650b8f49f31b87e169
Mr Z <denver.cfman@gmail.com>
2026-06-22 19:10:09 -0600

    Refactor boot.cmd for USB and console settings
    Updated boot.cmd to streamline USB initialization and console output.

--- boot.cmd
+++ boot.cmd
@@ -1,19 +1,11 @@
 # boot.cmd
-# 1. Initialize USB bus
+# Set multi-console output
+setenv stdout serial,vidconsole
+setenv stderr serial,vidconsole
+setenv stdin serial,usbkbd
+
+# Continue with boot
 usb start
-
-# 2. Force probing of all USB devices
-usb reset
-
-# 3. List devices to verify the network interface is bound
-# If this doesn't show an 'eth' device, the driver is missing
-dm tree
-
-# 4. Attempt to initialize the network interface
-# Some boards require this to bind the driver to the USB device
-setenv autoload no
 dhcp
-
-# 5. Proceed with PXE
 pxe get
 pxe boot
