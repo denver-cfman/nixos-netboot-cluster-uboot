06dc75b6524d4e0b47cdc3d421e3838b96b5915b
Mr Z <denver.cfman@gmail.com>
2026-06-24 19:59:24 -0600

    Update boot.cmd

--- boot.cmd
+++ boot.cmd
@@ -3,12 +3,23 @@
 setenv stdout serial,vidconsole
 setenv stderr serial,vidconsole
 setenv stdin serial,usbkbd
-
+echo "we need to wait 20 sec for the auto neg to work on the network switch"
 sleep 20
 usb start
 usb tree
 net list
-sleep 10
+echo "we need to wait 20 sec for the auto neg to work on the network switch"
+sleep 15
 dhcp
+# Example boot.cmd for debugging
+echo "--- Starting Netboot ---"
+echo "Setting up network..."
+dhcp
+echo "Network configured, attempting to load kernel..."
+tftpboot ${kernel_addr_r} kernel.img
+echo "Kernel loaded to ${kernel_addr_r}"
+booti ${kernel_addr_r} - ${fdt_addr_r}
+echo "Boot command executed!"
+echo "Verifying variables: IP=${ipaddr} Server=${serverip} Kernel=${kernel_addr_r}"
 pxe get
 pxe boot
